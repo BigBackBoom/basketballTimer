def8c9fe036a647f39a71816f5a9f9403d3d66d6
bigbackboom <kodaik@me.com>
2018-12-31 15:51:32 +0900

    add new class

---
+++ timer/Model/Util/SoundPlayModel.swift
@@ -0,0 +1,14 @@
+//
+//  SoundPlayModel.swift
+//  Sefo Timer
+//
+//  Created by 菊池広大 on 2018/12/31.
+//  Copyright © 2018 bigbackboom. All rights reserved.
+//
+
+import Foundation
+
+class SoundPlayModel {
+
+
+}
