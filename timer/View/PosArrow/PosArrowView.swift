eff25f725d01b37b869bdffc51309345e3627285
bigbackboom <kodaik@me.com>
2019-01-13 16:02:59 +0900

    some design has been added

--- timer/View/PosArrow/PosArrowView.swift
+++ timer/View/PosArrow/PosArrowView.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class PosArrowView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        loadNib()
     }
-    */
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        loadNib()
+    }
+    
+    func loadNib(){
+        let view = Bundle.main.loadNibNamed("PosArrow", owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
 
 }
