e4a541afa5613b6c0003f463b0020f30800fc3ec
bigbackboom <kodaik@me.com>
2019-01-13 15:18:10 +0900

    add possArrowView

---
+++ timer/View/PosArrow/PosArrowView.swift
@@ -0,0 +1,21 @@
+//
+//  PosArrowView.swift
+//  Sefo Timer
+//
+//  Created by 菊池広大 on 2019/01/13.
+//  Copyright © 2019 bigbackboom. All rights reserved.
+//
+
+import UIKit
+
+class PosArrowView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
